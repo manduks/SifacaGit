59ab24ab1cb31b592f63393e6ac63b9892d5e061
waldix <waldix86@gmail.com>
2012-05-15 01:00:15 -0500

    [ODLG] Se agrega configuracion para mail en produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,20 @@ Sifaca::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "waldix86@gmail.com",
+      password: "patito86"
+  }
+
+  config.action_mailer.default :charset => "utf-8"
 
   # Enable threaded mode
   # config.threadsafe!
