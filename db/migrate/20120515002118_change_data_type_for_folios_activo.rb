974d4e001ae680ab6542fe635a0003d0c2ccdb94
waldix <waldix86@gmail.com>
2012-05-14 21:10:51 -0500

    [ODLG] Folios activo

--- db/migrate/20120515002118_change_data_type_for_folios_activo.rb
+++ db/migrate/20120515002118_change_data_type_for_folios_activo.rb
@@ -1,6 +1,8 @@
 class ChangeDataTypeForFoliosActivo < ActiveRecord::Migration
   def up
-    change_column :folios, :activo, :integer
+    change_table :folios do |t|
+      t.change :activo, :integer
+    end
   end
 
   def down
